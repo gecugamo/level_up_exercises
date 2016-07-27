a4033c04daccde11ad246d1d1031dc25ad28f8af
Gary Cuga-Moylan <gcugamoylan@enova.com>
2016-07-26 21:20:39 -0500

    Add spaces after commas

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -1,9 +1,9 @@
 # Killer facts about triangles AWW YEAH
 class Triangle
-  attr_accessor :side1,:side2,:side3
+  attr_accessor :side1, :side2, :side3
 
-  def initialize(side1,side2,side3)
-    @side1,@side2,@side3 = side1,side2,side3
+  def initialize(side1, side2 ,side3)
+    @side1, @side2, @side3 = side1, side2, side3
   end
 
   def equilateral()
@@ -11,7 +11,7 @@ class Triangle
   end
 
   def isosceles()
-    return [side1,side2,side3].uniq.length == 2
+    return [side1, side2, side3].uniq.length == 2
   end
 
   def scalene()
@@ -24,10 +24,10 @@ class Triangle
 
   def recite_facts
     puts 'This triangle is equilateral!' if equilateral
-    puts 'This triangle is isosceles! Also, that word is hard to type.'     if isosceles
+    puts 'This triangle is isosceles! Also, that word is hard to type.' if isosceles
     puts 'This triangle is scalene and mathematically boring.' if scalene
 
-    angles = self.calculate_angles(side1,side2,side3)
+    angles = self.calculate_angles(side1, side2, side3)
     puts 'The angles of this triangle are ' + angles.join(',')
 
     puts 'This triangle is also a right triangle!' if angles.include? 90
@@ -49,8 +49,8 @@ end
 
 
 triangles = [
-  [5,5,5],
-  [5,12,13],
+  [5, 5, 5],
+  [5, 12, 13],
 ]
 triangles.each { |sides|
   tri = Triangle.new(*sides)
